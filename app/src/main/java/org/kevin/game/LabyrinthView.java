1ff81c4a65e7806f2802866abbe5f6d2e10df421
Kaiku0125 <pianoprince0125@gmail.com>
2021-08-08 21:02:27 +0800

    2021/8/8_add remove random walls

--- app/src/main/java/org/kevin/game/LabyrinthView.java
+++ app/src/main/java/org/kevin/game/LabyrinthView.java
@@ -58,12 +58,12 @@ public class LabyrinthView extends View {
     @SuppressLint("DrawAllocation")
     @Override
     protected void onDraw(Canvas canvas) {
-        Log.e(TAG, "onDraw: ");
+        Log.e(TAG, "onDraw: start");
         canvas.drawColor(Color.GREEN);
 
         int width = getWidth();
         int height = getHeight();
-        Log.e(TAG, "onDraw: width : " + width + ",height : " + height);
+//        Log.e(TAG, "onDraw: width : " + width + ",height : " + height);
 
         if(width / height < COLS / ROWS){
             cellSize = Math.round(width / (COLS + 1.0));
@@ -71,7 +71,7 @@ public class LabyrinthView extends View {
             cellSize = Math.round(height/(ROWS + 1.0));
         }
 
-        Log.e(TAG, "onDraw: cellSize : " + cellSize);
+//        Log.e(TAG, "onDraw: cellSize : " + cellSize);
         hMargin = (width - COLS * cellSize) / 2;
         vMargin = (height - ROWS * cellSize) / 2;
         if(hMargin < 0 || vMargin < 0){
@@ -79,7 +79,7 @@ public class LabyrinthView extends View {
             return;
         }
 
-        Log.e(TAG, "onDraw: " + hMargin + "," + vMargin);
+//        Log.e(TAG, "onDraw: " + hMargin + "," + vMargin);
 
         canvas.translate(hMargin, vMargin);
 
@@ -99,7 +99,7 @@ public class LabyrinthView extends View {
                 }
             }
         }
-
+        Log.e(TAG, "onDraw: currentDir" + currentDir.toString());
         if(currentDir == Direction.NONE){
             rectPlayer = new Rect(Math.round(player.col*cellSize),
                     Math.round(player.row*cellSize),
@@ -108,11 +108,7 @@ public class LabyrinthView extends View {
         }
         canvas.drawRect(rectPlayer, playerPaint);
 
-//        canvas.drawRect(player.col*cellSize,
-//                          player.row*cellSize,
-//                        (player.col+1)*cellSize,
-//                        (player.row+1)*cellSize,
-//                                playerPaint);
+
     }
 
     private void createLabyrinth(){
@@ -144,6 +140,8 @@ public class LabyrinthView extends View {
                 current = stack.pop();
             }
         } while(!stack.empty());
+
+        remove_random_walls(25);
 
     }
 
@@ -210,71 +208,253 @@ public class LabyrinthView extends View {
         executorService.execute(new Runnable() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(3000);
-                }catch (InterruptedException e){
-                    e.printStackTrace();
+//                try {
+//                    Thread.sleep(500);
+//                }catch (InterruptedException e){
+//                    e.printStackTrace();
+//                }
+                if(currentDir == Direction.NONE){
+                    try {
+                        Thread.sleep(2000);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
                 }
-                Log.e(TAG, "run: " + player.col + "," + player.row);
-
-
+                Log.e(TAG, "run start: " + player.col + "," + player.row);
 
                 checkNextPosition();
 
+                Log.e(TAG, "run end: " + player.col + "," + player.row);
 
-                Log.e(TAG, "run: " + player.col + "," + player.row);
-
-
+                Log.e(TAG, "_________________one move_____________________");
+                move();
             }
         });
     }
 
     private void checkNextPosition(){
+//        Log.e(TAG, "checkNextPosition: ");
+        ArrayList<Direction> directions = new ArrayList<>();
+
         if(!cells[player.col][player.row].topWall){
             Log.e(TAG, "run: top" );
 //            player = cells[player.col][player.row-1];
-            currentDir = Direction.UP;
+            directions.add(Direction.UP);
+//            currentDir = Direction.UP;
 
         }
         if(!cells[player.col][player.row].leftWall){
             Log.e(TAG, "run: left" );
 //            player = cells[player.col-1][player.row];
-            currentDir = Direction.LEFT;
+            directions.add(Direction.LEFT);
+//            currentDir = Direction.LEFT;
 
         }
         if(!cells[player.col][player.row].bottomWall){
             Log.e(TAG, "run: bottom" );
 //            player = cells[player.col][player.row+1];
-            currentDir = Direction.DOWN;
+            directions.add(Direction.DOWN);
+//            currentDir = Direction.DOWN;
 
         }
         if(!cells[player.col][player.row].rightWall){
             Log.e(TAG, "run: right" );
 //            player = cells[player.col+1][player.row];
-            currentDir = Direction.RIGHT;
+            directions.add(Direction.RIGHT);
+//            currentDir = Direction.RIGHT;
         }
+
+        int index = random.nextInt(directions.size());
+        currentDir = directions.get(index);
         goDir(currentDir);
+
     }
 
 
     private void goDir(Direction direction){
         switch (direction){
             case UP:
+                for(int i = 1; i<=2; i++){
+                    rectPlayer = new Rect(Math.round(player.col*cellSize),
+                            Math.round((player.row-(float)i/2)*cellSize),
+                            Math.round((player.col+1)*cellSize),
+                            Math.round(((player.row+1)-(float)i/2)*cellSize));
+
+                    post(new Runnable() {
+                        @Override
+                        public void run() {
+                            invalidate();
+                        }
+                    });
+                    try {
+                        Thread.sleep(500);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
                 player = cells[player.col][player.row-1];
-                rectPlayer = new Rect(Math.round(player.col*cellSize),
-                        Math.round((player.row-0.5f)*cellSize),
-                        Math.round((player.col+1)*cellSize),
-                        Math.round(((player.row+1)-0.5f)*cellSize));
-                invalidate();
+//                Log.e(TAG, "run test: " + player.col + "," + player.row);
+                break;
+
             case LEFT:
+                for(int i = 1; i<= 2; i++){
+                    rectPlayer = new Rect(Math.round((player.col - (float)i/2)*cellSize),
+                            Math.round((player.row)*cellSize),
+                            Math.round(((player.col+1)-(float)i/2)*cellSize),
+                            Math.round((player.row+1)*cellSize));
+
+                    post(new Runnable() {
+                        @Override
+                        public void run() {
+                            invalidate();
+                        }
+                    });
+                    try {
+                        Thread.sleep(500);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
                 player = cells[player.col-1][player.row];
+                break;
+
             case DOWN:
+                for(int i = 1; i<= 2; i++){
+                    rectPlayer = new Rect(Math.round(player.col*cellSize),
+                            Math.round((player.row + (float)i/2)*cellSize),
+                            Math.round((player.col+1)*cellSize),
+                            Math.round(((player.row+1) + (float)i/2)*cellSize));
+
+                    post(new Runnable() {
+                        @Override
+                        public void run() {
+                            invalidate();
+                        }
+                    });
+                    try {
+                        Thread.sleep(500);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
                 player = cells[player.col][player.row+1];
+                break;
             case RIGHT:
+                for(int i = 1; i<= 2; i++){
+                    rectPlayer = new Rect(Math.round((player.col + (float)i/2)*cellSize),
+                            Math.round((player.row)*cellSize),
+                            Math.round(((player.col+1) + (float)i/2)*cellSize),
+                            Math.round((player.row+1)*cellSize));
+
+                    post(new Runnable() {
+                        @Override
+                        public void run() {
+                            invalidate();
+                        }
+                    });
+                    try {
+                        Thread.sleep(500);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
                 player = cells[player.col+1][player.row];
+                break;
         }
 
     }
+
+    private void remove_diagonal_walls(){
+        for(int i = 2; i <= 10; i++){
+            Direction direction;
+            ArrayList<Direction> directions = new ArrayList<>();
+            if(cells[i][i].topWall){
+                directions.add(Direction.UP);
+            }
+            if(cells[i][i].leftWall){
+                directions.add(Direction.LEFT);
+            }
+            if(cells[i][i].bottomWall){
+                directions.add(Direction.DOWN);
+            }
+            if(cells[i][i].rightWall){
+                directions.add(Direction.RIGHT);
+            }
+            int index = random.nextInt(directions.size());
+            direction = directions.get(index);
+            switch (direction){
+                case UP:
+                    cells[i][i].topWall = false;
+                    cells[i][i-1].bottomWall = false;
+                    break;
+                case LEFT:
+                    cells[i][i].leftWall = false;
+                    cells[i-1][i].rightWall = false;
+                    break;
+                case DOWN:
+                    cells[i][i].bottomWall = false;
+                    cells[i][i+1].topWall = false;
+                    break;
+                case RIGHT:
+                    cells[i][i].rightWall = false;
+                    cells[i+1][i].leftWall = false;
+                    break;
+            }
+
+        }
+    }
+
+    private void remove_random_walls(int many){
+        for(int i = 0; i < many; i++){
+            Random rCol = new Random();
+            int col = rCol.nextInt(9) + 1;
+            Random rRow = new Random();
+            int row = rRow.nextInt(9) + 1;
+            Log.e(TAG, "remove_random_walls: col:" + col + "row" + row);
+            Direction direction;
+            ArrayList<Direction> directions = new ArrayList<>();
+            if(cells[col][row].topWall){
+                directions.add(Direction.UP);
+            }
+            if(cells[col][row].leftWall){
+                directions.add(Direction.LEFT);
+            }
+            if(cells[col][row].bottomWall){
+                directions.add(Direction.DOWN);
+            }
+            if(cells[col][row].rightWall){
+                directions.add(Direction.RIGHT);
+            }
+            if(directions.size() != 0){
+                int index = random.nextInt(directions.size());
+                Log.e(TAG, "remove_random_walls: index:" + index);
+                direction = directions.get(index);
+                switch (direction){
+                    case UP:
+                        cells[col][row].topWall = false;
+                        cells[col][row-1].bottomWall = false;
+                        break;
+                    case LEFT:
+                        cells[col][row].leftWall = false;
+                        cells[col-1][row].rightWall = false;
+                        break;
+                    case DOWN:
+                        cells[col][row].bottomWall = false;
+                        cells[col][row+1].topWall = false;
+                        break;
+                    case RIGHT:
+                        cells[col][row].rightWall = false;
+                        cells[col+1][row].leftWall = false;
+                        break;
+                }
+
+            }
+        }
+    }
+
+
+
+
 
 
 }
--- app/src/main/java/org/kevin/game/SecondActivity.java
+++ app/src/main/java/org/kevin/game/SecondActivity.java
@@ -2,15 +2,19 @@ package org.kevin.game;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class SecondActivity extends Activity {
+    LabyrinthView labyrinthView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_second);
+        labyrinthView = findViewById(R.id.view_labyrinth);
+        
     }
 }
